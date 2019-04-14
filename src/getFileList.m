5adb154773db14213cbaac70dd2af1b4e319ae3a
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2019-04-14 17:54:35 +0200

    Renamed deprecated function

--- src/getFileList.m
+++ src/getFileList.m
@@ -1,6 +1,6 @@
 function fileList = getFileList(directory)
     % Check to make sure that folder actually exists.  Warn user if it doesn't.
-    if ~isdir(directory)
+    if ~isfolder(directory)
         errorMessage = sprintf('Error: The following folder does not exist:\n%s', directory);
         uiwait(warndlg(errorMessage));
         return;
