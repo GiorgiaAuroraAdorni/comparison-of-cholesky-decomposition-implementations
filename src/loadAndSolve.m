641bfc93c03647b4889795d33a7cc8f154e56ad9
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2019-04-14 17:55:40 +0200

    Extracted line that solve the system ina new function in order to get the performance

---
+++ src/loadAndSolve.m
@@ -0,0 +1,14 @@
+function [e, n] = loadAndSolve(fileName)
+    matrix = load(fileName);
+    A = matrix.Problem.A;
+
+    n = size(A, 1);
+    xe = ones(n, 1);
+    b = A * xe;
+    
+    fprintf(1, 'Now solving %s\n', fileName);
+    x = solveSystem(A, b);
+
+    % precision or relative error
+    e = norm(x - xe) / norm(xe);  
+end
--- src/solveSystem.m
+++ src/solveSystem.m
@@ -1,12 +1,3 @@
-function [n, e] = solveSystem(fileName)
-    matrix = load(fileName);
-    A = matrix.Problem.A;
-
-    n = size(A, 1);
-    xe = ones(n, 1);
-    b = A * xe;
+function x = solveSystem(A, b)
     x = A \ b;
-
-    % precision or relative error
-    e = norm(x - xe) / norm(xe);  
 end
