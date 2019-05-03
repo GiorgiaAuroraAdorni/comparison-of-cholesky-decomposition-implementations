af48b27f89fc44630b6cb78e2d3cbb1ae75173e7
Elia Cereda <eliacereda@gmail.com>
2019-05-03 18:32:25 +0200

    Measure the load time and solve time for each matrix and write them to stdout

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -2,9 +2,21 @@
 
 #include <Eigen/Eigen>
 
+#include <boost/algorithm/string.hpp>
 #include <boost/filesystem.hpp>
 
+#include <chrono>
 #include <iostream>
+
+struct MatrixInfo {
+    Eigen::Index rows;
+    Eigen::Index nonZeros;
+
+    double relativeError;
+
+    std::chrono::duration<double, std::milli> loadTime;
+    std::chrono::duration<double, std::milli> solveTime;
+};
 
 template<typename SparseMatrixType>
 Eigen::VectorXd solveSystem(SparseMatrixType A, Eigen::VectorXd b) {
@@ -15,37 +27,84 @@ Eigen::VectorXd solveSystem(SparseMatrixType A, Eigen::VectorXd b) {
     return solver.solve(b);
 }
 
-void processMatrix(std::string filename) {
+MatrixInfo processMatrix(const std::string &filename) {
     using namespace Eigen;
 
-    SparseMatrix<double> matrix;
+    MatrixInfo info;
+    SparseMatrix<double> A;
 
-    std::cerr << "Processing matrix '" << filename << "'... " << std::flush;
+    {
+        std::cerr << "Loading matrix '" << filename << "'... " << std::flush;
 
-    bool result = loadMarketGzip(matrix, filename);
-    auto A = matrix.selfadjointView<Lower>();
+        auto start = std::chrono::high_resolution_clock::now();
 
-    std::cerr << (result ? "OK" : "ERROR") << std::endl;
-    std::cerr << A.rows() << "x" << A.cols() << std::endl;
+        SparseMatrix<double> matrix;
+        bool result = loadMarketGzip(matrix, filename);
 
-    VectorXd xe = VectorXd::Ones(A.rows());
-    VectorXd b = A * xe;
+        A = matrix.selfadjointView<Lower>();
 
-    VectorXd x = solveSystem(A, b);
+        auto end = std::chrono::high_resolution_clock::now();
 
-    double relativeError = (x - xe).norm() / xe.norm();
+        std::cerr << (result ? "OK" : "ERROR") << std::endl;
 
-    std::cerr << "Relative error: " << relativeError << std::endl;
+        info.rows = A.rows();
+        info.nonZeros = A.nonZeros();
+        info.loadTime = end - start;
+
+        std::cerr << "Rows: " << info.rows << std::endl;
+        std::cerr << "Non-zero elements: " << info.nonZeros << std::endl;
+    }
+
+    {
+        VectorXd xe = VectorXd::Ones(A.rows());
+        VectorXd b = A * xe;
+
+        std::cerr << "Solving system... " << std::flush;
+
+        auto start = std::chrono::high_resolution_clock::now();
+
+        VectorXd x = solveSystem(A, b);
+
+        auto end = std::chrono::high_resolution_clock::now();
+
+        std::cerr << "OK" << std::endl;
+
+        info.solveTime = end - start;
+        info.relativeError = (x - xe).norm() / xe.norm();
+
+        std::cerr << "Relative error: " << info.relativeError << std::endl;
+    }
+
+    return info;
 }
 
 int main(int argc, char **argv) {
     using namespace Eigen;
     namespace fs = boost::filesystem;
+    namespace algo = boost::algorithm;
 
     std::string directory = "prova";
 
+    std::cout << "name,rows,nonZeros,loadTime,solveTime,relativeError" << std::endl;
+
     for (auto &file : fs::directory_iterator(directory)) {
-        processMatrix(file.path().string());
+        fs::path filepath = file.path();
+
+        std::string filename = filepath.filename().string();
+        std::string extension = filepath.extension().string();
+
+        if (!algo::ends_with(filename, ".mtx.gz")) {
+            continue;
+        }
+
+        MatrixInfo info = processMatrix(filepath.string());
+
+        std::cout << filename << ","
+                  << info.rows << ","
+                  << info.nonZeros << ","
+                  << info.loadTime.count() << ","
+                  << info.solveTime.count() << ","
+                  << info.relativeError << std::endl;
     }
 
     return 0;
