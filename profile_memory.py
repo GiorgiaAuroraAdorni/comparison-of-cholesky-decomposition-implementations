79a710c8cba71a2971fae4d90c40e46e270deab3
Elia Cereda <eliacereda@gmail.com>
2019-05-04 12:21:46 +0200

    Updated profile_memory.py to sample memory at 20Hz and print more data

--- profile_memory.py
+++ profile_memory.py
@@ -2,6 +2,7 @@
 
 import psutil
 import sys
+import time
 
 pid = int(sys.argv[1])
 p = psutil.Process(pid)
@@ -10,6 +11,7 @@ initial_memory = p.memory_info().rss
 max_memory = 0
 
 print("Monitoring " + p.name() + "...")
+print("Initial memory usage:", initial_memory)
 
 try:
     while True:
@@ -18,9 +20,11 @@ try:
         if max_memory < current_memory:
             max_memory = current_memory
 
-except KeyboardInterrupt:
-    pass
+        time.sleep(0.050) # 20Hz sampling rate
+except:
+    print("Exception:", sys.exc_info()[0])
 
 memory_usage = max_memory - initial_memory
 
-print(memory_usage)
+print("Max memory usage:", max_memory)
+print("Delta memory usage:", memory_usage)
