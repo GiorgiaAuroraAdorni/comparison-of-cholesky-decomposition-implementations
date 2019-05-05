7c7c94e43e7f18158bde0ea4476b648d85d75ef5
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2019-05-05 13:11:58 +0200

    Extracted functions in util.py in order to use these in different script.
    Renamed profile_memory.py script to  profile_memory_matlab.py

--- profile_memory_matlab.py
+++ profile_memory_matlab.py
@@ -1,59 +1,14 @@
 #!/usr/bin/env python3
 
-import pandas as pd
+from util import get_file_list, monitor_memory
+
 import subprocess
-import psutil
-import time
 import sys
 import csv
 import os
 
+os.chdir("matlab")
 
-def monitor_memory(subproc):
-    pid = subproc.pid
-    proc = psutil.Process(pid)
-
-    initial_memory = proc.memory_info().rss
-    max_memory = 0
-
-    print("Monitoring " + proc.name() + "...")
-    print("Initial memory usage:", initial_memory)
-
-    try:
-        while subproc.poll() is None:
-            current_memory = proc.memory_info().rss
-
-            if max_memory < current_memory:
-                max_memory = current_memory
-
-            time.sleep(0.050)  # 20Hz sampling rate
-
-    except:
-        print("Exception:", sys.exc_info()[0])
-
-    memory_usage = max_memory - initial_memory
-
-    print("Max memory usage:", max_memory)
-    print("Delta memory usage:", memory_usage)
-
-    return max_memory, memory_usage
-
-
-def get_file_list(directory):
-    matrix_list = list()
-    file_list = list()
-
-    # Iterate over all the entries
-    for file in os.listdir(directory):
-        if file.endswith(".mat"):
-            matrix_list.append(file)
-            # Store full path
-            file_list.append(os.path.join(directory, file))
-
-    return file_list, matrix_list
-
-
-# FIXME
 matrices_dir = "matrices"
 
 file_list, matrix_list = get_file_list(matrices_dir)
